e787c37cda326215d809d32843b281892dba1d26
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-21 19:32:30 +0100

    xx

--- app.js
+++ app.js
@@ -11,19 +11,48 @@ function startApp() {
 }
 
 function addToDo() {
-  const myHTML = /*html*/ `
+  if (newToDo.value.trim() != "") {
+    const myHTML = /*html*/ `
     <li> 
     <span>${newToDo.value}</span>
-    <button>Delete</button>
-    </li>`;
-  list.insertAdjacentHTML("beforeEnd", myHTML);
-  newToDo.value = "";
+    <button class="delete">Delete</button>
+    <button class="edit">Edit</button>
 
-  list
-    .querySelector("li:last-child button")
-    .addEventListener("click", removeToDo);
+    
+    </li>`;
+    list.insertAdjacentHTML("beforeEnd", myHTML);
+    newToDo.value = "";
+
+    list
+      .querySelector("li:last-child button.delete")
+      .addEventListener("click", removeToDo);
+
+    list
+      .querySelector("li:last-child button.edit")
+      .addEventListener("click", editToDo);
+  } else {
+    newToDo.value = "";
+    newToDo.focus();
+  }
 }
 
 function removeToDo() {
   this.parentNode.remove();
+}
+
+function editToDo() {
+  console.log(this);
+  this.parentNode.querySelector("span").setAttribute("contenteditable", true);
+  this.removeEventListener("click", editToDo);
+  this.textContent = "save";
+  this.addEventListener("click", sayToDo);
+  this.parentNode.querySelector("span").focus();
+  console.log(this.parentNode);
+}
+
+function sayToDo() {
+  this.parentNode.querySelector("span").setAttribute("contenteditable", false);
+  this.removeEventListener("click", sayToDo);
+  this.textContent = "Edit";
+  this.addEventListener("click", editToDo);
 }
